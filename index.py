19500e3dba9fd5f6eb6da1a0f281a007a0ff5993
Kader1680 <ouldhenniabaghdad@gmail.com>
2024-10-20 22:39:14 +0100

    update code

--- index.py
+++ index.py
@@ -9,11 +9,18 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
 # Find all <p> tags with the class 'text'
-paragraphs = soup.find_all("p", attrs={"class": "paragraph"})
+paragraphs = soup.find_all("strong")
 
 # Print the text from each paragraph
-for p in paragraphs:
-    print(p)
-    
-    
 
+file = open("scrapping_data.txt", "a")
+
+for p in paragraphs:
+    
+    file.write(p.text)
+    
+    
+file.close()
+
+file = open("scrapping_data.txt", "r")
+print(file.read())
